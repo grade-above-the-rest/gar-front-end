17019dc58d259cdddbfee39454bbd18e20df67ac
Nembius <thenembius@gmail.com>
2019-03-28 14:18:18 -0500

    added mail.js and contact.html

---
+++ mail.js
@@ -0,0 +1,28 @@
+
+var mailer = require('nodemailer');
+
+var mailInfo = mailer.createTransport({
+		service: 'gmail',
+		auth: {
+    user: 'gradeabovetherest@gmail.com',
+    pass: '@9H5N26B'
+  }
+});
+
+
+var mailMessage = {
+  from: document.write(document.getElementById("Email").value,
+  to: 'gradeabovetherest@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: document.write(document.getElementById("First_Name").value + "<br>" + 
+						document.getElementById("Last_Name").value) + "<br><br>" +
+						document.getElementById("Message_Box").value) + "<br><br>") 
+};
+
+mailInfo.sendMail(mailMessage, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 

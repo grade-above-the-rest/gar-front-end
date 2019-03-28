35ec24f1386da1933f644afe0534470adcbba317
Nembius <thenembius@gmail.com>
2019-03-28 14:55:24 -0500

    new update

--- mail.js
+++ mail.js
@@ -19,10 +19,15 @@ var mailMessage = {
 						document.getElementById("Message_Box").value) + "<br><br>") 
 };
 
-mailInfo.sendMail(mailMessage, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-}); 
+
+
+function data()
+{
+	mailInfo.sendMail(mailMessage, function(error, info){
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Email sent: ' + info.response);
+	}
+	}); 	
+}
